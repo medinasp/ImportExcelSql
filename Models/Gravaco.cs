94bec1ad98c086c4611f675ebdd560764ba5e117
Eneias <medinasp@gmail.com>
2021-11-19 12:24:20 -0300

    Initial Commit

---
+++ Models/Gravaco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ImportExcelSql.Models
+{
+    public partial class Gravaco
+    {
+        public int Id { get; set; }
+        public int? IdBancoCliente { get; set; }
+        public string Numero { get; set; }
+        public string NomeDoArquivo { get; set; }
+        public int? FileSize { get; set; }
+        public string FilePath { get; set; }
+        public string Ramal { get; set; }
+
+        public virtual Bancocliente IdBancoClienteNavigation { get; set; }
+    }
+}
---
+++ Models/StaffInfoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportExcelSql.Models
+{
+    public class StaffInfoViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<StaffInfoViewModel> StaffList { get; set; }
+        public StaffInfoViewModel()
+        {
+            StaffList = new List<StaffInfoViewModel>();
+        }
+    }
+}
