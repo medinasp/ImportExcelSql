2fa6afdb1247a0d675b4f3f190f522849db0ac5f
André Santos <andre.santos@kantar.com>
2021-12-01 16:40:27 -0300

    Review - Importar conteúdo do excel após visualizar em tabela html

---
+++ Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ImportExcelSql.Models
+{
+    public partial class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
